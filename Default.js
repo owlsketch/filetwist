e9c594bb91bb74eaa83d7c64d9c8dfa381d5fe92
Helen See <helen.see@rutgers.edu>
2015-04-18 16:16:44 -0400

    upload pt1
    upload button works

--- Default.js
+++ Default.js
@@ -1,8 +1,13 @@
 
-
-
-function handleFiles(selection){
-	var files = selection.target.files;
-	
-	
-}
+function handleFileSelections(selection) {
+	var files = selection.target.files; 
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+      output.push('<li>', escape(f.name), ' (', f.type || 'n/a', ') - ',
+                  f.size, ' bytes, last modified: ',
+                  f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+                  '</li>');
+    }
+   
+  }
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
