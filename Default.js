b6ea954c8ed94b3b23f705377bd9106c5f7760df
Satimidus <satimidus@gmail.com>
2015-04-18 19:30:30 -0400

    support for webMs now

--- Default.js
+++ Default.js
@@ -1,6 +1,3 @@
-
-
-
   function handleFileSelect(selection) {
     var files = selection.target.files; 
 	
@@ -8,9 +5,31 @@
     for (var i = 0, f; f = files[i]; i++) {
 
       if (!f.type.match('image.*')) {
-        continue;
+        var extension = f.name.split('.')[1];
+        if(extension == "pdf"){
+        console.log("yay");
+        }
+        else if(extension == "webm"){
+            var reader = new FileReader();
+
+            reader.onload = (function(theFile) {
+            return function(e) {
+			//prints images in divs with specific format
+          var div = document.createElement('div');
+          div.innerHTML = ['<video class="webms" src="', e.target.result, '" title="',e.target.result,'" controls></video>'].join('');
+           document.getElementById('container').insertBefore(div, null);
+        };
+      })(f);
+
+      reader.readAsDataURL(f);
+            // <div>
+            // <video class="webms" src="name.webm" title="name.webm" controls></video>
+            // </div>
+            //
+      }
       }
 
+      else {
       var reader = new FileReader();
 
       reader.onload = (function(theFile) {
@@ -25,6 +44,7 @@
 
       reader.readAsDataURL(f);
     }
-  }
+  }}
 
   document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
