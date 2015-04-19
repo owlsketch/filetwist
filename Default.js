17941ea7251c4febca618b0e35f6dabfc6074aaf
Helen See <helen.see@rutgers.edu>
2015-04-18 23:12:44 -0400

    spacing fixed

--- Default.js
+++ Default.js
@@ -15,47 +15,48 @@ function handleFileSelect(selection) {
 	//goes through list of chosen files
     for (var i = 0, f; f = files[i]; i++) {
 	    storeFiles(f);
-      if (!f.type.match('image.*')) {
-        var extension = f.name.split('.')[1];
-        if(extension == "pdf"){
-        console.log("yay");
-        }
-        else if(extension == "webm"|| extension == "mkv" || extension == "mp4"){
-            var reader = new FileReader();
+		if (!f.type.match('image.*')) {
+			var extension = f.name.split('.')[1];
+			if(extension == "pdf"){
+				console.log("yay");
+			}
+			else if(extension == "webm"|| extension == "mkv" || extension == "mp4"){
+				var reader = new FileReader();
 
-            reader.onload = (function(theFile) {
-            return function(e) {
-			//prints images in divs with specific format
-            var div = document.createElement('div');
-            div.setAttribute("width", "1px");
-            div.setAttribute("height","1px");
-            div.innerHTML = ['<video class="webms" src="', e.target.result, '" title="Video" controls></video>'].join('');
-            document.getElementById('container').insertBefore(div, null);
-        };
-      })(f);
+				reader.onload = (function(theFile) {
+					return function(e) {
+						//prints images in divs with specific format
+						var div = document.createElement('div');
+						div.setAttribute("width", "1px");
+						div.setAttribute("height","1px");
+						div.innerHTML = ['<video class="webms" src="', e.target.result, '" title="Video" controls></video>'].join('');
+						document.getElementById('container').insertBefore(div, null);
+					};
+				})(f);
 
-      reader.readAsDataURL(f);
-      }
-      }
+				reader.readAsDataURL(f);
+			}
+		}
 
-      else {
-      var reader = new FileReader();
+		else {
+			var reader = new FileReader();
 
-      reader.onload = (function(theFile) {
+			reader.onload = (function(theFile) {
 
-        return function(e) {
-			//prints images in divs with specific format
-			var div = document.createElement('div');
-            div.setAttribute("width", "1px");
-            div.setAttribute("height","1px");
-			div.innerHTML = ['<img class="thumb" src="', e.target.result,
-							'" title="', escape(theFile.name), '"/>'].join('');
-			document.getElementById('container').insertBefore(div, null);
-			};
-		})(f);
+				return function(e) {
+					//prints images in divs with specific format
+					var div = document.createElement('div');
+					div.setAttribute("width", "1px");
+					div.setAttribute("height","1px");
+					div.innerHTML = ['<img class="thumb" src="', e.target.result,
+									'" title="', escape(theFile.name), '"/>'].join('');
+					document.getElementById('container').insertBefore(div, null);
+					};
+			})(f);
 
-    reader.readAsDataURL(f);
-    }
-  }}
+			reader.readAsDataURL(f);
+		}
+	}
+}
 
 document.getElementById('files').addEventListener('change', handleFileSelect, false);
